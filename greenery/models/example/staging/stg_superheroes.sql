9a481e40e5881edd6e178ecfc226ff5fe99ff995
Matt Whiteley <thatmattwhiteley@gmail.com>
2022-03-07 00:47:07 +0000

    Week 1 additions: stg_superheroes, weight_lbs_to_kgs, tests, dbt_utils packages

---
+++ greenery/models/example/staging/stg_superheroes.sql
@@ -0,0 +1,19 @@
+{{
+    config(materialized='table'
+    )
+}}
+
+SELECT
+    id as superhero_id
+    ,name
+    ,gender
+    ,eye_color
+    ,race
+    ,hair_color
+    ,NULLIF(height,-99) as height
+    ,publisher
+    ,skin_color
+    ,alignment
+    ,NULLIF(weight,-99) as weight_lbs
+    ,{{ lbs_to_kgs('weight') }} as weight_kg
+FROM {{source('tutorial','superheroes')}}
